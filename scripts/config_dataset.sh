2e147fbcf956af158acb2884fd795190e055020e
RijuDey <RijuDey@node0.riju-d.e6897-pg0.utah.cloudlab.us>
2025-03-05 13:07:13 -0700

    script to run multiple benchmarks

--- scripts/config_dataset.sh
+++ scripts/config_dataset.sh
@@ -3,9 +3,9 @@
 # Switch dataset in the config_local.sh file by calling the desired function
 
 dataset_spacev1b_100m() {
-  BASE_PATH=/users/yuhong/nvme1n1/SPTAG/datasets/SPACEV1B/vectors.bin/vectors_merged.bin
-  QUERY_FILE=/users/yuhong/nvme1n1/SPTAG/datasets/SPACEV1B/query.bin
-  GT_FILE=/users/yuhong/nvme1n1/SPTAG/datasets/SPACEV1B/truth_100m.bin
+  BASE_PATH=~/new_partition/SPTAG/datasets/SPACEV1B/vectors.bin/vectors_merged.bin
+  QUERY_FILE=~/new_partition/SPTAG/datasets/SPACEV1B/query.bin
+  GT_FILE=~/new_partition/SPTAG/datasets/SPACEV1B/truth_100m.bin
   PREFIX=spacev1b_100m
   DATA_TYPE=int8
   DIST_FN=l2
--- scripts/config_local.sh
+++ scripts/config_local.sh
@@ -24,15 +24,15 @@ USE_SQ=0
 MEM_R=64
 MEM_BUILD_L=128
 MEM_ALPHA=1.2
-MEM_RAND_SAMPLING_RATE=0.01  # randomly sample 1% to construct in-memory graph
-MEM_USE_FREQ=0
-MEM_FREQ_USE_RATE=0.01
+MEM_RAND_SAMPLING_RATE=0.02
+MEM_USE_FREQ=1
+MEM_FREQ_USE_RATE=0.02
 
 ##########################
 #   Generate Frequency   #
 ##########################
 FREQ_QUERY_FILE=$QUERY_FILE
-FREQ_QUERY_CNT=0 # Set 0 to use all (default)
+FREQ_QUERY_CNT=1000
 FREQ_BM=4
 FREQ_L=100 # only support one value at a time for now
 FREQ_T=16
