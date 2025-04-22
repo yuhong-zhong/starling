cb74e702f3fe2242426484edce3c88371fd449b0
RijuDey <RijuDey@node0.rijudey-253077.e6897-pg0.clemson.cloudlab.us>
2025-04-22 17:08:35 -0400

    experiment C

---
+++ scripts/experimentC.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Path to the summary log
+SUMMARY_PATH=../indices/summary.log
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# Clear summary file
+rm -f $SUMMARY_PATH
+
+# Parameter values to test
+MEM_USE_FREQ=1
+SAMPLING_RATE=0
+FREQ_QUERY_CNT=29316
+FREQ_QUERY_FILE="~/new_partitions/SPTAG/datasets/query_partitions/query_part_1.bin"
+CACHE=0
+# Memory Frequency values to test
+MEM_FREQ_USE_RATES=(0.1 0.01 0.001 0.0001)
+
+
+# Backup original config file
+cp config_local.sh config_local.sh.bak
+
+# Experiment with different cache values
+for MEM_FREQ_USE_RATE in "${MEM_FREQ_USE_RATES[@]}"; do
+    # Update config_local.sh with different values
+    sed -i "s/^MEM_RAND_SAMPLING_RATE=.*/MEM_RAND_SAMPLING_RATE=$SAMPLING_RATE/" config_local.sh
+    sed -i "s/^FREQ_QUERY_CNT=.*/FREQ_QUERY_CNT=$FREQ_QUERY_CNT/" config_local.sh
+    sed -i "s/^MEM_USE_FREQ=.*/MEM_USE_FREQ=$MEM_USE_FREQ/" config_local.sh
+    sed -i "s/^CACHE=.*/CACHE=$CACHE/" config_local.sh
+    sed -i "s/^FREQ_QUERY_FILE=.*/CACHE=$FREQ_QUERY_FILE/" config_local.sh
+    sed -i "s/^MEM_FREQ_USE_RATE=.*/MEM_FREQ_USE_RATE=$MEM_FREQ_USE_RATE/" config_local.sh
+
+    # Output the current settings
+    printf "${GREEN}Running with MEM_USE_FREQ=$MEM_USE_FREQ, FREQ_QUERY_CNT=$FREQ_QUERY_CNT, CACHE=$CACHE${NC}\n"
+    cat config_local.sh
+
+    # Run benchmark tests (uncomment to run)
+    ./run_benchmark.sh release freq knn
+    ./run_benchmark.sh release build_mem
+    ./run_benchmark.sh release search knn
+done
+
+# Display the updated configuration
+
+
+# Restore the original config file
+mv config_local.sh.bak config_local.sh
+
+# Print summary
+printf "${GREEN}Summary${NC}\n"
+cat $SUMMARY_PATH
